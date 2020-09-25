c92da4dbeca078f75cd4a0754f0b444f4fe66f62
mattlongman <git@mattlongman.com>
2020-09-25 12:15:23 +0100

    Set logging level
    Add debug config option. debug=0 for mininal output. debug=1 to show addon detail. debug=2 for same as 1 + run hostapd in debug mode.

--- hassio-access-point/run.sh
+++ hassio-access-point/run.sh
@@ -2,14 +2,21 @@
 
 # SIGTERM-handler this funciton will be executed when the container receives the SIGTERM signal (when stopping)
 term_handler(){
-	echo "Stopping..."
+	logger "Stopping Hass.io Access Point" 0
 	ifdown $INTERFACE
 	ip link set $INTERFACE down
 	ip addr flush dev $INTERFACE
 	exit 0
 }
 
-echo "Starting..."
+# Logging function to set verbosity of output to addon log
+logger(){
+    msg=$1
+    level=$2
+    if [ $DEBUG -ge $level ]; then
+        echo $msg
+    fi
+}
 
 CONFIG_PATH=/data/options.json
 
@@ -26,51 +33,44 @@ DHCP_START_ADDR=$(jq --raw-output ".dhcp_start_addr" $CONFIG_PATH)
 DHCP_END_ADDR=$(jq --raw-output ".dhcp_end_addr" $CONFIG_PATH)
 ALLOW_MAC_ADDRESSES=$(jq --raw-output '.allow_mac_addresses | join(" ")' $CONFIG_PATH)
 DENY_MAC_ADDRESSES=$(jq --raw-output '.deny_mac_addresses | join(" ")' $CONFIG_PATH)
+DEBUG=$(jq --raw-output '.debug' $CONFIG_PATH)
 
 # Set interface as wlan0 if not specified in config
 if [ ${#INTERFACE} -eq 0 ]; then
     INTERFACE="wlan0"
 fi
 
+# Set interface as wlan0 if not specified in config
+if [ ${#DEBUG} -eq 0 ]; then
+    DEBUG=0
+fi
+
+echo "Starting Hass.io Access Point Addon"
+
+# Setup interface
+logger "# Setup interface:" 1
+logger "Add to /etc/network/interfaces: iface $INTERFACE inet static" 1
 # Create and add our interface to interfaces file
 echo "iface $INTERFACE inet static"$'\n' >> /etc/network/interfaces
 
-echo "Set nmcli managed no"
+logger "Run command: nmcli dev set $INTERFACE managed no" 1
 nmcli dev set $INTERFACE managed no
+
+logger "Run command: ip link set $INTERFACE down" 1
+ip link set $INTERFACE down
+
+logger "Add to /etc/network/interfaces: address $ADDRESS" 1
+echo "address $ADDRESS"$'\n' >> /etc/network/interfaces
+logger "Add to /etc/network/interfaces: netmask $NETMASK" 1
+echo "netmask $NETMASK"$'\n' >> /etc/network/interfaces
+logger "Add to /etc/network/interfaces: broadcast $BROADCAST" 1
+echo "broadcast $BROADCAST"$'\n' >> /etc/network/interfaces
+
+logger "Run command: ip link set $INTERFACE up" 1
+ip link set $INTERFACE up
 
 # Setup signal handlers
 trap 'term_handler' SIGTERM
-
-### MAC address filtering
-## Allow is more restrictive, so we prioritise that and set
-## macaddr_acl to 1, and add allowed MAC addresses to hostapd.allow
-if [ ${#ALLOW_MAC_ADDRESSES} -ge 1 ]; then
-    echo "macaddr_acl=1"$'\n' >> /hostapd.conf
-    ALLOWED=($ALLOW_MAC_ADDRESSES)
-    for mac in "${ALLOWED[@]}"; do
-        echo "$mac"$'\n' >> /hostapd.allow
-    done
-    echo "accept_mac_file=/hostapd.allow"$'\n' >> /hostapd.conf
-## else set macaddr_acl to 0, and add denied MAC addresses to hostapd.deny
-    else
-        if [ ${#DENY_MAC_ADDRESSES} -ge 1 ]; then
-            echo "macaddr_acl=0"$'\n' >> /hostapd.conf
-            DENIED=($DENY_MAC_ADDRESSES)
-            for mac in "${DENIED[@]}"; do
-                echo "$mac"$'\n' >> /hostapd.deny
-            done
-            echo "deny_mac_file=/hostapd.deny"$'\n' >> /hostapd.conf
-## else set macaddr_acl to 0, with blank allow and deny files
-            else
-                echo "macaddr_acl=0"$'\n' >> /hostapd.conf
-        fi
-
-fi
-
-
-
-# Add interface to hostapd.conf
-echo "interface=$INTERFACE"$'\n' >> /hostapd.conf
 
 # Enforces required env variables
 required_vars=(SSID WPA_PASSPHRASE CHANNEL ADDRESS NETMASK BROADCAST)
@@ -83,7 +83,7 @@ done
 
 # Sanitise config value for hide_ssid
 if [ $HIDE_SSID -ne 1 ]; then
-        HIDE_SSID=0
+    HIDE_SSID=0
 fi
 
 # Sanitise config value for dhcp
@@ -96,39 +96,78 @@ if [[ -n $error ]]; then
 fi
 
 # Setup hostapd.conf
-echo "Setup hostapd ..."
+logger "# Setup hostapd:" 1
+logger "Add to hostapd.conf: ssid=$SSID" 1
 echo "ssid=$SSID"$'\n' >> /hostapd.conf
+logger "Add to hostapd.conf: wpa_passphrase=********" 1
 echo "wpa_passphrase=$WPA_PASSPHRASE"$'\n' >> /hostapd.conf
+logger "Add to hostapd.conf: channel=$CHANNEL" 1
 echo "channel=$CHANNEL"$'\n' >> /hostapd.conf
+logger "Add to hostapd.conf: ignore_broadcast_ssid=$HIDE_SSID" 1
 echo "ignore_broadcast_ssid=$HIDE_SSID"$'\n' >> /hostapd.conf
 
-# Setup dnsmasq.conf if DHCP is enabled in config
-echo "Setup dnsmasq ..."
-if [ $DHCP -eq 1 ]; then
-        echo "dhcp-range=$DHCP_START_ADDR,$DHCP_END_ADDR,12h"$'\n' >> /dnsmasq.conf
-        echo "interface=$INTERFACE"$'\n' >> /dnsmasq.conf
-	else
-	echo "DHCP not enabled"
+### MAC address filtering
+## Allow is more restrictive, so we prioritise that and set
+## macaddr_acl to 1, and add allowed MAC addresses to hostapd.allow
+if [ ${#ALLOW_MAC_ADDRESSES} -ge 1 ]; then
+    logger "Add to hostapd.conf: macaddr_acl=1" 1
+    echo "macaddr_acl=1"$'\n' >> /hostapd.conf
+    ALLOWED=($ALLOW_MAC_ADDRESSES)
+    logger "# Setup hostapd.allow:" 1
+    logger "Allowed MAC addresses:" 0
+    for mac in "${ALLOWED[@]}"; do
+        echo "$mac"$'\n' >> /hostapd.allow
+        logger "$mac" 0
+    done
+    logger "Add to hostapd.conf: accept_mac_file=/hostapd.allow" 1
+    echo "accept_mac_file=/hostapd.allow"$'\n' >> /hostapd.conf
+## else set macaddr_acl to 0, and add denied MAC addresses to hostapd.deny
+    else
+        if [ ${#DENY_MAC_ADDRESSES} -ge 1 ]; then
+            logger "Add to hostapd.conf: macaddr_acl=0" 1
+            echo "macaddr_acl=0"$'\n' >> /hostapd.conf
+            DENIED=($DENY_MAC_ADDRESSES)
+            logger "Denied MAC addresses:" 0
+            for mac in "${DENIED[@]}"; do
+                echo "$mac"$'\n' >> /hostapd.deny
+                logger "$mac" 0
+            done
+            logger "Add to hostapd.conf: accept_mac_file=/hostapd.deny" 1
+            echo "deny_mac_file=/hostapd.deny"$'\n' >> /hostapd.conf
+## else set macaddr_acl to 0, with blank allow and deny files
+            else
+                logger "Add to hostapd.conf: macaddr_acl=0" 1
+                echo "macaddr_acl=0"$'\n' >> /hostapd.conf
+        fi
+
 fi
 
-# Setup interface
-echo "Setup interface ..."
 
-## extra killall thrown in - not required? ## killall hostapd
 
-ip link set $INTERFACE down
+# Add interface to hostapd.conf
+logger "Add to hostapd.conf: interface=$INTERFACE" 1
+echo "interface=$INTERFACE"$'\n' >> /hostapd.conf
 
-## move this ^ ## echo "iface $INTERFACE inet static"$'\n' >> /etc/network/interfaces
-echo "address $ADDRESS"$'\n' >> /etc/network/interfaces
-echo "netmask $NETMASK"$'\n' >> /etc/network/interfaces
-echo "broadcast $BROADCAST"$'\n' >> /etc/network/interfaces
-
-ip link set $INTERFACE up
+# Setup dnsmasq.conf if DHCP is enabled in config
+if [ $DHCP -eq 1 ]; then
+    logger "# DHCP enabled. Setup dnsmasq:" 1
+    logger "Add to dnsmasq.conf: dhcp-range=$DHCP_START_ADDR,$DHCP_END_ADDR,12h" 1
+        echo "dhcp-range=$DHCP_START_ADDR,$DHCP_END_ADDR,12h"$'\n' >> /dnsmasq.conf
+        logger "Add to dnsmasq.conf: interface=$INTERFACE" 1
+        echo "interface=$INTERFACE"$'\n' >> /dnsmasq.conf
+	else
+	logger "# DHCP not enabled. Skipping dnsmasq" 1
+fi
 
 # Start dnsmasq if DHCP is enabled in config
 if [ $DHCP -eq 1 ]; then
-	killall dnsmasq; dnsmasq -C /dnsmasq.conf
+    logger "## Starting dnsmasq daemon" 1
+	killall -q dnsmasq; dnsmasq -C /dnsmasq.conf
 fi
 
-echo "Starting HostAP daemon ..."
-killall hostapd; hostapd -d /hostapd.conf & wait ${!}
+logger "## Starting hostapd daemon" 1
+if [ $DEBUG -gt 1 ]; then
+    killall -q hostapd; hostapd -d /hostapd.conf & wait ${!}
+else
+    killall -q hostapd; hostapd /hostapd.conf & wait ${!}
+fi
